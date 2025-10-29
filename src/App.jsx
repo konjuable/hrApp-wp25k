f6d496aa39357ab81e08168f6707be8a0ceef91f
konjuable <juha.konttinen@bc.fi>
2025-10-29 10:38:44 +0200

    Tilanne 1. viikon jälkeen 22.10.2025.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Person from "./components/Person"
 
-function App() {
+const App = () => {
 
   return (
     <>
-      <p>App komponentti</p>
+      <Header />
+      <Person 
+        name="Juha Konttinen" 
+        title="Lecturer"
+        salary="10 €"
+        phone="040 730 6004"
+        email="juha.konttinen@bc.fi"
+        pet="dog"
+      />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>2025 ©️ Etunimi Sukunimi - WP25K</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
